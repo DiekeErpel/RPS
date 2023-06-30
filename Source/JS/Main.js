c76ec2c1a2a2c7cf5e3cf74aa939a491846411cf
Wesley Suylen <wesleysuylen@gmail.com>
2023-06-30 13:55:13 +0200

    RPS Main JS Functions

--- Source/JS/Main.js
+++ Source/JS/Main.js
@@ -6,50 +6,71 @@ const rockElement = document.getElementById("steen");
 const paperElement = document.getElementById("papier");
 const scissorsElement = document.getElementById("schaar");
 
-const RPS = ["steen", "papier", "schaar"];
-
 const reset = document.getElementById("reset");
 
 const playerScore = document.getElementById("score");
 const computerScore = document.getElementById("computerScore");
 
+var userChoice = "";
+var computerChoice = "";
 
-
-// Computer choice
-let computerChoice = Math.floor(Math.random() * 3);
-
-// Displaying the computer choice
-if (computerChoice == 0) {
-    computerChoice = "steen";
-} else if (computerChoice == 1) {
-    computerChoice = "papier";
-} else {
-    computerChoice = "schaar";
+// Computer choice function
+function getComputerChoice() {
+    var choice = Math.floor(Math.random() * 3);
+    if (choice === 0) {
+        return "steen";
+    } else if (choice === 1) {
+        return "papier";
+    } else {
+        return "schaar";
+    }
 }
-
-console.log(computerChoice);
 
 
 
 // Event listerners for the buttons
-
 rockElement.addEventListener("click", function () {
-    console.log("Steen");
+    userChoice = "steen";
+    computerChoice = getComputerChoice();
+    if (computerChoice === 'steen') {
+        ties++; console.log("Tie");
+    } else if (computerChoice === 'papier') {
+        losses++; console.log("Loss");
+    } else {
+        wins++; console.log("Win");
+    }
 });
 
 paperElement.addEventListener("click", function () {
-    console.log("Papier");
+    userChoice = "papier";
+    computerChoice = getComputerChoice();
+    if (computerChoice === 'papier') {
+        ties++; console.log("Tie");
+    } else if (computerChoice === 'schaar') {
+        losses++; console.log("Loss");
+    } else {
+        wins++; console.log("Win");
+    }
 });
 
 scissorsElement.addEventListener("click", function () {
-    console.log("Schaar");
+    userChoice = "schaar";
+    computerChoice = getComputerChoice();
+    if (computerChoice === 'schaar') {
+        ties++; console.log("Tie");
+    } else if (computerChoice === 'steen') {
+        losses++; console.log("Loss");
+    } else {
+        wins++; console.log("Win");
+    }
 });
-
-
 
 // Event listerners for the reset
-
 reset.addEventListener("click", function () {
     console.log("Reset");
+    var wins = 0;
+    var ties = 0;
+    var losses = 0;
 });
+
 
