5c97bac5f5e0db565b076a29853111c5d5206b74
Wesley Suylen <wesleysuylen@gmail.com>
2023-06-30 12:41:37 +0200

    Fixes

--- Source/JS/Main.js
+++ Source/JS/Main.js
@@ -2,26 +2,46 @@ var wins = 0;
 var ties = 0;
 var losses = 0;
 
-const rock = document.getElementById("steen");
-const paper = document.getElementById("papier");
-const scissors = document.getElementById("schaar");
+const rockElement = document.getElementById("steen");
+const paperElement = document.getElementById("papier");
+const scissorsElement = document.getElementById("schaar");
+
+const RPS = ["steen", "papier", "schaar"];
 
 const reset = document.getElementById("reset");
 
 const playerScore = document.getElementById("score");
+const computerScore = document.getElementById("computerScore");
+
+
+
+// Computer choice
+let computerChoice = Math.floor(Math.random() * 3);
+
+// Displaying the computer choice
+if (computerChoice == 0) {
+    computerChoice = "steen";
+} else if (computerChoice == 1) {
+    computerChoice = "papier";
+} else {
+    computerChoice = "schaar";
+}
+
+console.log(computerChoice);
+
 
 
 // Event listerners for the buttons
 
-rock.addEventListener("click", function () {
+rockElement.addEventListener("click", function () {
     console.log("Steen");
 });
 
-paper.addEventListener("click", function () {
+paperElement.addEventListener("click", function () {
     console.log("Papier");
 });
 
-scissors.addEventListener("click", function () {
+scissorsElement.addEventListener("click", function () {
     console.log("Schaar");
 });
 
